c8f1b3fec88cfe4fd2179202dfef892272a1034f
David Rojas <deznorth@gmail.com>
2021-12-02 16:09:28 -0500

    Started the form with some sections and buttons

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-
-console.log('The script was loaded successfully!');
+function handleSubmit() {
+  const form = document.getElementById('Form');
+  // Object containing all form data keyed by their "name" property
+  const formData = Object.fromEntries(new FormData(form));
+  console.log(formData);
+}
+
